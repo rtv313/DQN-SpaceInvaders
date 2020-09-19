c9e135d78aab744b8f581cc7cf1e16317b698a2e
rtv313.isc <rtv313.isc@hotmail.com>
2020-09-19 09:32:05 -0500

    fix bug for rewards

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-
 import numpy as np
-from deep_q_network import  DeepQNetwork
+from deep_q_network import DeepQNetwork
 from environment_manager import EnvManager
 from epsilon_greedy_strategy import EpsilonGreedyStrategy
 from agent import Agent
-from replay_memory import ReplayMemory,Experience
+from replay_memory import ReplayMemory, Experience
 
+render = False
 batch_size = 160
-gamma = 0.999 # Is the discount factor used in the Bellman equation
-eps_start = 1 # Starting value of epsilon
-eps_end = 0.1 # Ending value of epsilon
-eps_decay = 0.001 # Decay rate we’ll use to decay epsilon over time
-target_update = 10 # How frequently, in terms of episodes, we’ll update the target network weights with the policy network weights.
-memory_size = 2000 # Capacity of the replay memory
-lr = 0.001 # Learning rate
-num_episodes = 1000 # Number of episodes we want to play
+gamma = 0.999  # Is the discount factor used in the Bellman equation
+eps_start = 1  # Starting value of epsilon
+eps_end = 0.1  # Ending value of epsilon
+eps_decay = 0.001  # Decay rate we’ll use to decay epsilon over time
+target_update = 10  # How frequently, in terms of episodes, we’ll update the target network weights with the policy network weights.
+memory_size = 2000  # Capacity of the replay memory
+lr = 0.001  # Learning rate
+num_episodes = 1000  # Number of episodes we want to play
 
 environment_manager = EnvManager('SpaceInvaders-v0')
 strategy = EpsilonGreedyStrategy(eps_start, eps_end, eps_decay)
 agent = Agent(strategy, environment_manager.num_actions_available())
 memory = ReplayMemory(memory_size)
-policy_net = DeepQNetwork(input_shape=(environment_manager.get_input_shape(),), action_space = environment_manager.num_actions_available(),batch_size = batch_size)
-target_net = DeepQNetwork(input_shape=(environment_manager.get_input_shape(),), action_space = environment_manager.num_actions_available(),batch_size = batch_size)
+policy_net = DeepQNetwork(input_shape=(environment_manager.get_input_shape(),),
+                          action_space=environment_manager.num_actions_available(), batch_size=batch_size)
+target_net = DeepQNetwork(input_shape=(environment_manager.get_input_shape(),),
+                          action_space=environment_manager.num_actions_available(), batch_size=batch_size)
 
 episode_max_rewards = []
-
 # Episode loop
 for episode in range(num_episodes):
     max_episode_reward = 0
@@ -36,7 +37,9 @@ for episode in range(num_episodes):
     steps = 0
     while not environment_manager.done:
 
-        environment_manager.render()
+        if reward == True:
+            environment_manager.render()
+
         action = agent.select_action(state, policy_net)
         experience = environment_manager.take_action(action)
         state = experience[0]
@@ -45,13 +48,13 @@ for episode in range(num_episodes):
         reward = experience[3]
         steps += 1
         memory.push(Experience(state, action, next_state, reward))
-        max_episode_reward = reward if (reward > max_episode_reward) else max_episode_reward
+        max_episode_reward += reward
         state = next_state
 
         if memory.can_provide_sample(batch_size):
             experiences_batch = memory.sample(batch_size)
-            states = np.zeros((batch_size,environment_manager.final_reshape))
-            next_states = np.zeros((batch_size,environment_manager.final_reshape))
+            states = np.zeros((batch_size, environment_manager.final_reshape))
+            next_states = np.zeros((batch_size, environment_manager.final_reshape))
             actions, rewards = [], []
 
             # Prepare data batch
